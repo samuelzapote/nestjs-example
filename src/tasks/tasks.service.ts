27c06ffef9031f4ce5c652fd1ea7574cdb0db739
Samuel Zapote Granjeno <samuel@zapote.org>
2020-10-09 10:46:05 -0500

    feat(Tasks): Get task with provided id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,6 +12,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  public getTaskById(id: string): Task {
+    return this.tasks.find(t => t.id === id);
+  }
+
   public createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {

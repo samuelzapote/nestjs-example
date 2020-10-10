b3cfcc6189d979bf5d891f35f5c2243e8932caeb
Samuel Zapote Granjeno <samuel@zapote.org>
2020-10-10 16:38:14 -0500

    feat(Tasks): Get tasks with a filter
    Refactors the get all tasks functionality to use this feature.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,14 +3,19 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestj
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) { }
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Get('/:id')
@@ -29,7 +34,7 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
+  deleteTaskById(@Param('id') id: string): void {
     this.tasksService.deleteTaskById(id);
   }
 }
